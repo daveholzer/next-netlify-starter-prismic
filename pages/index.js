deb76b16b4411b932279159e0d11d253f98576ed
David Holzer <35435899+daveholzer@users.noreply.github.com>
2021-05-24 16:03:44 -0700

    Initial

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { client } from '@util/prismicPosts'
+import Post from '@components/Post'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// inside your component markup, pull `posts` from props
+<div className="posts">
+  {posts !== undefined &&
+    posts.map((p) => {
+      let title = p.title[0].text
+      let key = `${p.date}+${title}`
+      return <Post key={key} date={p.date} image={p.image} title={title} />
+    })}
+</div>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+// at the bottom of your component file
+export async function getStaticProps() {
+  // query() is empty on purpose!
+  // https://prismic.io/docs/rest-api/query-the-api/query-all-documents
+  const res = await client.query('')
 
-      <Footer />
-    </div>
-  )
+  const posts = res.results.map((p) => {
+    return p.data
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
