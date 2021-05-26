6604d47bf2259b376f3531a16b1ec7a43c213c9d
David Holzer <35435899+daveholzer@users.noreply.github.com>
2021-05-26 08:48:09 -0700

    Fix typo

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,70 @@
-import { client } from '@util/prismicPosts'
+import Head from 'next/head'
+
+import { client } from '../util/prismicPost'
+
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 import Post from '@components/Post'
 
-// inside your component markup, pull `posts` from props
-<div className="posts">
-  {posts !== undefined &&
-    posts.map((p) => {
-      let title = p.title[0].text
-      let key = `${p.date}+${title}`
-      return <Post key={key} date={p.date} image={p.image} title={title} />
-    })}
-</div>
+export default function Home({ posts }) {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next + Prismic Starter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-// at the bottom of your component file
+      <main>
+        <Header />
+        <div className="posts">
+          {posts !== undefined &&
+            posts.map((p) => {
+              let title = p.title[0].text
+              let key = `${p.date}+${title}`
+              return <Post key={key} date={p.date} image={p.image} title={title} />
+            })}
+        </div>
+      </main>
+
+      <Footer />
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .posts {
+          display: flex;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
+
 export async function getStaticProps() {
   // query() is empty on purpose!
   // https://prismic.io/docs/rest-api/query-the-api/query-all-documents
